68ff448dbb79d47fbc1e1191bbd7413c74fdefbf
unknown <rezamsanitrrga@gmail.com>
2023-12-23 19:28:45 +0330

    1402_10_02

---
+++ Application/Interfaces/Contexts/IDataBaseContext.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Catalogs;
+using Domain.Entities;
+using Domain.Order;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces.Contexts
+{
+    public interface IDataBaseContext
+    {
+        DbSet<CatalogBrand> catalogBrands { get; set; }
+        DbSet<CatalogType> catalogTypes { get; set; }
+        DbSet<CatalogItem> catalogItems { get; set; }
+        DbSet<CatalogItemFeature> CatalogItemFeature { get; set; }
+        DbSet<CatalogItemImage> CatalogItemImage { get; set; }
+        DbSet<Basket> Baskets { get; set; }
+        DbSet<BasketItem> BasketItems { get; set; }
+        DbSet<UserAddress> userAddresses { get; set; }
+        DbSet<OrderItem> OrderItems { get; set; }
+        DbSet<Order> Orders { get; set; }
+        int SaveChanges();
+        int SaveChanges(bool acceptAllChangesOnSuccess);
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+        Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default);
+
+    }
+}
---
+++ WebSite.EndPoint/Controllers/BasketController.cs
@@ -0,0 +1,85 @@
+﻿using Application.BasketsService;
+using Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebSite.EndPoint.Utilities;
+
+namespace WebSite.EndPoint.Controllers
+{
+    public class BasketController : Controller
+    {
+        private readonly IBasketsService basketService;
+        private readonly SignInManager<User> signInManager;
+        private string userId = null;
+        public BasketController(IBasketsService basketService
+            , SignInManager<User> signInManager)
+        {
+            this.basketService = basketService;
+            this.signInManager = signInManager;
+        }
+        public IActionResult Index()
+        {
+            var data = GetOrSetBasket();
+            return View(data);
+        }
+
+
+        [HttpPost]
+        public IActionResult Index(int CatalogitemId, int quantity = 1)
+        {
+            var basket = GetOrSetBasket();
+            basketService.AddItemToBasket(basket.Id, CatalogitemId, quantity);
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        [HttpPost]
+        public IActionResult RemoveItemFromBasket(int ItemId)
+        {
+            basketService.RemoveItemFromBasket(ItemId);
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        public IActionResult setQuantity(int basketItemId, int quantity)
+        {
+            return Json(basketService.SetQuantities(basketItemId, quantity));
+        }
+
+
+        private BasketDto GetOrSetBasket()
+        {
+            if (signInManager.IsSignedIn(User))
+            {
+                userId = ClaimUtility.GetUserId(User);
+                return basketService.GetOrCreateBasketForUser(userId);
+            }
+            else
+            {
+                SetCookiesForBasket();
+                return basketService.GetOrCreateBasketForUser(userId);
+            }
+        }
+
+        private void SetCookiesForBasket()
+        {
+            string basketCookieName = "BasketId";
+            if (Request.Cookies.ContainsKey(basketCookieName))
+            {
+                userId = Request.Cookies[basketCookieName];
+            }
+            if (userId != null) return;
+            userId = Guid.NewGuid().ToString();
+            var cookieOptions = new CookieOptions { IsEssential = true };
+            cookieOptions.Expires = DateTime.Today.AddYears(2);
+            Response.Cookies.Append(basketCookieName, userId, cookieOptions);
+
+
+        }
+    }
+}
